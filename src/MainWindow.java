6358e9c0aa21136e4201336c509f8dbfdd3cb0b0
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 15:50:49 +0200

    Der Button verändert die Schriftgröße des oben angezeigten Texts.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
 
@@ -7,10 +9,23 @@ public class MainWindow extends JFrame {
         setTitle("Mein Fenster");
         setSize(400, 250);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
         setLayout( new BorderLayout() );
-        add( new JLabel("Das ist langweiliger Text"), BorderLayout.NORTH );
-        add( new JButton("Button"), BorderLayout.SOUTH );
+
+        JLabel meinText = new JLabel("Das ist langweiliger Text");
+        JButton meinButton = new JButton("Button");
+        add( meinText, BorderLayout.NORTH );
+        add( meinButton, BorderLayout.SOUTH );
+        meinButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                meinText.setFont(
+                        new Font(
+                                meinText.getFont().getFontName(),
+                                meinText.getFont().getStyle(),
+                                meinText.getFont().getSize()+1
+                        ));
+            }
+        });
 
         setVisible(true);
     }
