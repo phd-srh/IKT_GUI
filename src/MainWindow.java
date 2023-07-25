4c13163e7f80134d7063c6ab42108b1a58de9717
Dr. Dillinger <peter.dillinger@srh.de>
2023-07-25 15:55:25 +0200

    Button Action via Funktionales Interface realisert.

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
+    private final JLabel meinText;
 
     public MainWindow() {
         setTitle("Mein Fenster");
@@ -11,22 +12,21 @@ public class MainWindow extends JFrame {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLayout( new BorderLayout() );
 
-        JLabel meinText = new JLabel("Das ist langweiliger Text");
+        meinText = new JLabel("Das ist langweiliger Text");
         JButton meinButton = new JButton("Button");
         add( meinText, BorderLayout.NORTH );
         add( meinButton, BorderLayout.SOUTH );
-        meinButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                meinText.setFont(
-                        new Font(
-                                meinText.getFont().getFontName(),
-                                meinText.getFont().getStyle(),
-                                meinText.getFont().getSize()+1
-                        ));
-            }
-        });
+        meinButton.addActionListener( this::buttonKlicked );
 
         setVisible(true);
+    }
+
+    void buttonKlicked(ActionEvent e) {
+        meinText.setFont(
+                new Font(
+                        meinText.getFont().getFontName(),
+                        meinText.getFont().getStyle(),
+                        meinText.getFont().getSize()-1
+                ));
     }
 }
